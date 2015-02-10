4265c0b1f8a912a45117b12827550c3166356690
andrew74b <andrew74b@gmail.com>
2015-02-09 19:40:54 -0500

    start reporting functions

--- BackEnd/Reporting/Reporting.fs
+++ BackEnd/Reporting/Reporting.fs
@@ -3,7 +3,38 @@
 open System
 open Sands.Helpers
 
-type getReport (source) =
-    let sourceReport = 
-    let a =1
+type compareReport =
+    {
+        Site: string;
+        DateString: string;
+        FileCount: int;
+        Message: string;
+    }
+
+type getReport (site, source, destination, days:float, extension) =
+    let sourceReport source = 
+        Sands.Helpers.GetWorklist.getSiteStatus site source extension days 
+
+    let destinationReport source = 
+        Sands.Helpers.GetWorklist.getSiteStatus site destination extension days
     
+    let determineStatus status = 
+        if status.Status = false then
+            {
+                Site = status.Site;
+                DateString = status.Date;
+                FileCount = 0;
+                Message = "Source directory does not exist";
+            }
+        else
+            {
+                Site = status.Site;
+                DateString = status.Date;
+                FileCount = 0;
+                Message = "Source directory does not exist";           
+            }
+
+    member this.getReport() = 
+        sourceReport source
+        |> List.map (fun (status) -> "d")
+
